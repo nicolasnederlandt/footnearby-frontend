22dae145d3669b3d24f12a2950349f52ff577006
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-11-24 20:25:26 +0100

    add - webpack files

--- test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let btn1 = document.getElementById("myBtn1");
-
-btn1.onclick = function () {
-    btn1.innerText = "You clicked on me : )";
-    console.log("btn.onclick::anonymous function");
-  };
